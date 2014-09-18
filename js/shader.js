2f37753cb5f568f3040330b0037c88a510f81161
Sterling Hamilton <sterling.hamilton@gmail.com>
2014-09-18 13:24:11 -0700

    Update main script with better logic for background changing.

--- js/shader.js
+++ js/shader.js
@@ -45,15 +45,6 @@ $(function() {
             }
         }
 
-        //initBackground();
-    }
-
-    function isHighDensity(){
-      return ((window.matchMedia && (window.matchMedia('only screen and (min-resolution: 124dpi), only screen and (min-resolution: 1.3dppx), only screen and (min-resolution: 48.8dpcm)').matches || window.matchMedia('only screen and (-webkit-min-device-pixel-ratio: 1.3), only screen and (-o-min-device-pixel-ratio: 2.6/2), only screen and (min--moz-device-pixel-ratio: 1.3), only screen and (min-device-pixel-ratio: 1.3)').matches)) || (window.devicePixelRatio && window.devicePixelRatio > 1.3));
-    }
-
-    function isRetina(){
-        return ((window.matchMedia && (window.matchMedia('only screen and (min-resolution: 192dpi), only screen and (min-resolution: 2dppx), only screen and (min-resolution: 75.6dpcm)').matches || window.matchMedia('only screen and (-webkit-min-device-pixel-ratio: 2), only screen and (-o-min-device-pixel-ratio: 2/1), only screen and (min--moz-device-pixel-ratio: 2), only screen and (min-device-pixel-ratio: 2)').matches)) || (window.devicePixelRatio && window.devicePixelRatio > 2)) && /(iPad|iPhone|iPod)/g.test(navigator.userAgent);
     }
 
     function isRetinaDisplay() {
@@ -68,11 +59,14 @@ $(function() {
     }
 
     if(isRetinaDisplay()) {
+      $('body').attr('class','theme-sea');
       config.background.LIGHT.ambient = '#76E4CE';
       config.background.LIGHT.diffuse = '#0E411F';
       config.background.LIGHT.zOffset = 100;
+      initBackground();
       $('#message').html('Yes!');
     } else {
+      $('body').attr('class','theme-fire');
       initBackground();
       $('#message').html('No.');
     }
